5cff4e08a9ee405f6e9960a352c2f120a179a9de
Ajeeth706 <151006100+Ajeeth706@users.noreply.github.com>
2023-11-28 10:41:50 +0530

    Add files via upload

---
+++ Conversion/Program.cs
@@ -0,0 +1,19 @@
+﻿class Conversion
+{
+    static void Main()
+    {
+        int a = 25;
+        double b = (double)a;
+        float c = (float)b;
+        long d = (long)a;
+
+        Console.WriteLine("int:" + a);
+        Console.WriteLine("int:" + a.GetType());
+        Console.WriteLine("double:" + b);
+        Console.WriteLine("double:" + b.GetType());
+        Console.WriteLine("float:" + c);
+        Console.WriteLine("float:" + c.GetType());
+        Console.WriteLine("long:" + d);
+        Console.WriteLine("long:" + d.GetType());
+    }
+}
